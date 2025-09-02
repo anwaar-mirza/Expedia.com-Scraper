797d4538aa824653fef054e733cd2559a3ceceb5
anwaar-mirza <anwaar@metaviz.pro>
2025-09-02 10:34:55 +0500

    fgdd

--- ExpediaGroup.py
+++ ExpediaGroup.py
@@ -282,7 +282,7 @@ def handle_threading(thread_id, group_id, url_file_path, output_file_path):
             for id, url in enumerate(reader, start=group_id):
                 i+=1
                 print(f"Thread {thread_id}: Processing ID: {id}, Current Link: {i}, URL: {url[0]}")
-                bot.land_targeted_page(url)
+                bot.land_targeted_page(url=url)
                 bot.combine_all_sets(id=id, file_path=output_file_path, url=url)
 
 

e57f30971929fbb844183dfbe1f23ba83883778e
anwaar-mirza <anwaar@metaviz.pro>
2025-09-02 10:26:31 +0500

    dgdfs

--- ExpediaGroup.py
+++ ExpediaGroup.py
@@ -1,6 +1,7 @@
+import csv
 from playwright.sync_api import sync_playwright
 from geopy.geocoders import ArcGIS
-from openpyxl import load_workbook
+from threading import Thread
 from Essential import GenericMethods
 import pandas as pd
 import time
@@ -270,114 +271,26 @@ class ExpediaGroup(GenericMethods):
                     df.to_excel(writer, sheet_name=sheet, index=False)
 
 
-urls_list = [
-    # "https://www.expedia.com/Harare-Hotels-Sharon-Las-Palmas-Guest-House.h95375990.Hotel-Information?",
-    # "https://www.expedia.com/Bouvet-Island-Hotels-Vrbo-Property.h48288270.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-Cheltenham-Park-Resort.h101320617.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-The-Peech-Boutique-Hotel.h100845560.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-Marcifen.h115148261.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-Cottage-In-Harare-Near-Westgate.h111835722.Hotel-Information?",
-    # "https://www.expedia.com/Dakeni-Game-Farm.h114408699.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-Silver-Moon-Guest-House.h112412703.Hotel-Information?",
-    # "https://www.expedia.com/Bulawayo-Hotels-Gwango-Tamarillo-Estate.h113584537.Hotel-Information?",
-    # "https://www.expedia.com/Bouvet-Island-Hotels-Charming-One-Bedroom-Apartment.h109821359.Hotel-Information?",
-    # "https://www.expedia.com/Juliasdale-Hotels-Rupurara-Valley-Lodge.h109574152.Hotel-Information?",
-    # "https://www.expedia.com/Chinhoyi-Hotels-Welcome-To-The-Zebra-Room.h82989578.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-The-Victoria-Falls-Hotel.h24943.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-Kafusi-Lodge.h101433636.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-Shearwater-Explorers-Village.h18030489.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-Kasambabezi-Lodge.h116783828.Hotel-Information?",
-    # "https://www.expedia.com/Gweru-Hotels-Quaint-Cabin-With-WiFi-In-Charming-Gweru.h117832256.Hotel-Information?",
-    # "https://www.expedia.com/Goromonzi-Hotels-Pachigomo-Guest-House.h40052203.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-The-Telescope-Boutique-Lodge.h114264914.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-Cozy-And-Stylish-Room.h108985235.Hotel-Information?",
-    # "https://www.expedia.com/Matopos-Hotels-Matobo-Hills-Lodge.h2150542.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-Malcolm-Lodge.h15475962.Hotel-Information?",
-    # "https://www.expedia.com/Bulawayo-Hotels-Neat-One-Bedroom-In-Morningside-Guesthouse-2091.h94944869.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-Family-Friendly-Environment-With-A-Big-Garden-And-Barbeque-Area.h111135410.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-The-Manor-House.h116523247.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-The-Courtney-Lodge.h93663273.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-258-Hotel.h110380298.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-Phezulu-Lodge.h23665555.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-Shailoh-Folyjon-2Bedroomed-Wing.h33056835.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-Wild-Trekkers-Lodge.h10636191.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-1-Bed-Apartment-In-Mount-Pleasant-Heights-2014.h89962429.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-2-Bed-Apartment-With-En-Suite-Kitchenette-2064.h94945673.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-Marts-Place-Full-Solar-Power-Wi-Fi.h95698581.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-Vrbo-Property.h41656253.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-Residence-Hotel.h93528792.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-528-Victoria-Falls-Guest-House.h36026343.Hotel-Information?",
-    # "https://www.expedia.com/Mutare-Hotels-2-Bedroomed-House-With-A-Lovely-Garden-2177.h100192417.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-Amakhosi-Hotel.h112603051.Hotel-Information?",
-    # "https://www.expedia.com/Kariba-Hotels-Warthogs-Safari-Camp.h23986992.Hotel-Information?",
-    # "https://www.expedia.com/Zvishavane-Hotels-Enchanted-Africa-Apartments.h113578341.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-Togara-Guest-House.h41653260.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-2-Bedroomed-Apartment-With-En-Suite-And-Kitchenette-2066.h94943394.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-Lulu-Guest-Lodge.h96840734.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-Elephant-Hills-Resort.h536395.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-Vrbo-Property.h41656251.Hotel-Information?",
-    # "https://www.expedia.com/Bulawayo-Hotels-UPhondo-Lodge.h114141769.Hotel-Information?",
-    # "https://www.expedia.com/Kadoma-Hotels-PaLuana-Lodges-Kadoma.h115990705.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-Mt-Pleasant-Bed-And-Breakfast.h110719126.Hotel-Information?",
-    # "https://www.expedia.com/Masvingo-Hotels-Luxury-3-Bedroom-Self-Catering-Apartment-Masvingo.h103075359.Hotel-Information?",
-    # "https://www.expedia.com/Hurungwe-Hotels-Spring-Resort.h99931398.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-The-Hayhill-Treetop-Escape-Apartment-Offers-Scenic-Views-Overlooking-Harare.h119450942.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-The-Best-Green-Garden-Guest-House-In-Harare.h75432989.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-Liam-Villas-Newlands.h115156772.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-Cresta-Jameson.h6220.Hotel-Information?",
-    # "https://www.expedia.com/Beatrice-Hotels-Beatrice-Lodges-And-Conference-Centre.h30055311.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-Liam-Victoria-Falls.h95240288.Hotel-Information?",
-    # "https://www.expedia.com/1-Bed-Apartment-In-Mount-Pleasant-Heights-2013.h94944758.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-Stephen-Margolis-Resort.h27364660.Hotel-Information?",
-    # "https://www.expedia.com/Bulawayo-Hotels-Cozy-2-Bed-2-Bath-Apartment.h102313129.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-Grosvenor-House.h88324018.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-Amazing-Apartment-With-WiFi-In-Vibrant-Harare.h115094057.Hotel-Information?",
-    # "https://www.expedia.com/Bulawayo-Hotels-Motsamai-Lodge.h12696840.Hotel-Information?",
-    # "https://www.expedia.com/Harare-Hotels-2-Bed-Roomed-Fully-Furnished-And-Secure-Apartment.h37207486.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-Drift-Inn.h91341587.Hotel-Information?",
-    # "https://www.expedia.com/Victoria-Falls-Hotels-Dumisa-African-Home.h48392669.Hotel-Information?",
-    "https://www.expedia.com/Victoria-Falls-Hotels-Bhejane-Lodge.h92330586.Hotel-Information?",
-    "https://www.expedia.com/Bulawayo-Hotels-Holiday-Inn-Bulawayo.h49292.Hotel-Information?",
-    "https://www.expedia.com/Bulawayo-Hotels-Neat-Standard-Room-In-Guesthouse-2088.h94944238.Hotel-Information?",
-    "https://www.expedia.com/Harare-Hotels-Private-And-Charming-Room-In-Avondale.h106043272.Hotel-Information?",
-    "https://www.expedia.com/Victoria-Falls-Hotels-Miombo-Mews.h93413186.Hotel-Information?",
-    "https://www.expedia.com/Harare-Hotels-Cresta-Lodge-Harare.h18938322.Hotel-Information?",
-    "https://www.expedia.com/Harare-Hotels-Highlands-Lodges-And-Apartments.h21955172.Hotel-Information?",
-    "https://www.expedia.com/Harare-Hotels-Solar-Powered-3-Bed-Haven-In-Bannatyne-Park.h109935467.Hotel-Information?",
-    "https://www.expedia.com/Harare-Hotels-The-Hilltop-Guesthouse.h103834870.Hotel-Information?",
-    "https://www.expedia.com/Harare-Hotels-Casa-Favorita.h108701295.Hotel-Information?",
-    "https://www.expedia.com/Mount-Hampden-Hotels-CARTM-Apartments.h115695768.Hotel-Information?",
-    "https://www.expedia.com/Harare-Hotels-1-Bed-Apartment-In-Mount-Pleasant-Heights-2013.h89962431.Hotel-Information?",
-    "https://www.expedia.com/Harare-Hotels-N1-Hotel-Samora-Machel-Harare.h10478758.Hotel-Information?",
-    "https://www.expedia.com/Harare-Hotels-The-Country-Club.h113949543.Hotel-Information?",
-    "https://www.expedia.com/Bulawayo-Hotels-3-Bedroom-House-In-Charming-Bulawayo-With-WiFi.h115969997.Hotel-Information?",
-    "https://www.expedia.com/Mutare-Hotels-Madrugada-Magic.h23192238.Hotel-Information?",
-    "https://www.expedia.com/Harare-Hotels-Luxurious-2-Bedroom-Apartment-In-Harare-North.h116317042.Hotel-Information?",
-    "https://www.expedia.com/Gweru-Hotels-3-Bed-Cozy-Homestay-For-Groups-In-Lundi-Park-2292.h115384875.Hotel-Information?",
-    "https://www.expedia.com/Victoria-Falls-Hotels-Bhejane-Lodge.h92330586.Hotel-Information?",
-    "https://www.expedia.com/Victoria-Falls-Hotels-Victoria-Falls-Rainbow-Hotel.h2150531.Hotel-Information?",
-    "https://www.expedia.com/Bulawayo-Hotels-Breeze-Guest-House.h24282938.Hotel-Information?",
-    "https://www.expedia.com/Victoria-Falls-Hotels-Fidelity-Lodgings-And-Travel.h107066976.Hotel-Information?",
-    "https://www.expedia.com/Victoria-Falls-Hotels-Shongwe-Lookout.h34280264.Hotel-Information?",
-    "https://www.expedia.com/Harare-Hotels-Starz-Place-In-The-Avenues.h118078703.Hotel-Information?",
-    "https://www.expedia.com/Victoria-Falls-Hotels-Bethel-Lodgings.h103422690.Hotel-Information?",
-    "https://www.expedia.com/Harare-Hotels-2-Bed-Apartment-With-Ensuite-Kitchenette-2071.h94943739.Hotel-Information?",
-    "https://www.expedia.com/Harare-Hotels-2-Bed-Guesthouse-In-Mabelreign-2012.h94943736.Hotel-Information?",
-    "https://www.expedia.com/Bulawayo-Hotels-The-N1-Hotel-Bulawayo.h10483770.Hotel-Information?",
-    "https://www.expedia.com/Kotwa-Hotels-The-Pumpkin-Hotel.h6609995.Hotel-Information?",
-    "https://www.expedia.com/Bulawayo-Hotels-Musketeers-Lodge.h5494473.Hotel-Information?",
-    "https://www.expedia.com/Victoria-Falls-Hotels-Victoria-Falls-Oasis-Hotel.h117821672.Hotel-Information?",
-    "https://www.expedia.com/Macheke-Lodges-Conference-Centre.h45162241.Hotel-Information?",
-    "https://www.expedia.com/Harare-Hotels-Pemabwe.h93416448.Hotel-Information?",
-    "https://www.expedia.com/Harare-Hotels-Cosy-2-Bedroom-Retreat-With-Garden.h115856072.Hotel-Information?",
-    "https://www.expedia.com/Dete-Hotels-Hwange-Safari-Lodge.h1159499.Hotel-Information?",
-    "https://www.expedia.com/Harare-Hotels-Graceland-Guesthouse.h40913090.Hotel-Information?"
 
-]
+def handle_threading(group_id, url_file_path, output_file_path):
+    bot = ExpediaGroup()
+    if os.path.exists(url_file_path):
+        with open(url_file_path, 'r') as f:
+            next(f)
+            reader = csv.reader(f)
+            for id, url in enumerate(reader, start=group_id):
+                bot.land_targeted_page(url)
+                bot.combine_all_sets(id=id, file_path=output_file_path, url=url)
 
-group_id = int(input("Enter Group ID starting number: "))
-file_path = input("Enter file path with file name and extension (e.g., D:/ScrapingProjects/ExpediaGroup/output.xlsx): ")
-bot = ExpediaGroup()
-for id, url in enumerate(urls_list, start=group_id):
-    bot.land_targeted_page(url)
-    bot.combine_all_sets(id=id, file_path=file_path, url=url)
+
+th1 = Thread(target=handle_threading, args=(424, r"C:\Users\anwaa\Downloads\1_100.csv", 'ExpediaGroup_1.xlsx'))
+th1.start()
+th2 = Thread(target=handle_threading, args=(524, r"C:\Users\anwaa\Downloads\100_200_links.csv", 'ExpediaGroup_2.xlsx'))
+th2.start()
+th3 = Thread(target=handle_threading, args=(624,r"C:\Users\anwaa\Downloads\200_300 (1).csv" , 'ExpediaGroup_3.xlsx'))
+th3.start()
+
+
+th1.join()
+th2.join()
+th3.join()
